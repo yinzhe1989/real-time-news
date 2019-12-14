f44cf9b20644a6e97c2446bb73b5553be2b90406
shihaojie1219 <shihaojie1219@gmail.com>
2019-12-14 13:41:17 +0800

    pytohn path

--- hack/run.sh
+++ hack/run.sh
@@ -3,7 +3,7 @@ hackdir=$(cd $(dirname $0); pwd)
 workdir=$hackdir/..
 export PYTHONPATH=$workdir
 echo 'crawling news...'
-python3 $workdir/rtnews/crawl/async_crawl.py
+`which python3` $workdir/rtnews/crawl/async_crawl.py
 echo 'generating news html...'
-python3 $workdir/rtnews/feed/async_newsevent.py
+`which python3` $workdir/rtnews/feed/async_newsevent.py
 echo 'done'
