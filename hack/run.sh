31bcdf62b32c863f723be611122856f8ec541a36
shihaojie1219 <shihaojie1219@gmail.com>
2019-12-14 17:14:00 +0800

    add file to baiduyun

--- hack/run.sh
+++ hack/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+source /etc/profile
+export PATH=/usr/local/bin:/usr/bin:$PATH
 hackdir=$(cd $(dirname $0); pwd)
 workdir=$hackdir/..
 export PYTHONPATH=$workdir
@@ -6,4 +8,7 @@ echo 'crawling news...'
 `which python3` $workdir/rtnews/crawl/async_crawl.py
 echo 'generating news html...'
 `which python3` $workdir/rtnews/feed/async_newsevent.py
+echo 'uploading to baiduyun...'
+cd $workdir/dat
+`which bypy` upload
 echo 'done'
